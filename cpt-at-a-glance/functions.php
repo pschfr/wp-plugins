8f3b1be0e10cf60c3d0d982f39fb3a3412dbb09f
Paul Schaefer <yeppaul2@gmail.com>
2016-12-18 14:08:40 -0500

    added custom post type icons to CPT widget

--- cpt-at-a-glance/functions.php
+++ cpt-at-a-glance/functions.php
@@ -14,13 +14,21 @@ function cpt_at_a_glance() {
 		'_builtin' => false
 	), 'object', 'and');
 
+	if (current_user_can('edit_posts')) {
+		echo "<style>
+			#dashboard_right_now li a:before { content: ''; }
+			#dashboard_right_now li div.wp-menu-image { display: inline; }
+			#dashboard_right_now li div.wp-menu-image:before { color: #a0a5aa; padding: 0; }
+		</style>\n";
+	}
+
 	foreach ($post_types as $post_type) {
 		$num_posts = wp_count_posts($post_type->name);
 		$num = number_format_i18n($num_posts->publish);
 		$text = _n($post_type->labels->singular_name, $post_type->labels->name, intval($num_posts->publish));
 		if (current_user_can('edit_posts')) {
 			$output = '<a href="edit.php?post_type=' . $post_type->name . '">' . $num . ' ' . $text . '</a>';
-			echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
+			echo '<li><div class="wp-menu-image dashicons-before ' . $post_type->menu_icon . '"></div>' . $output . '</li>';
 		}
 	}
 }
